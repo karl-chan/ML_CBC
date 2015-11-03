3cf21b9607d70fbca239a57d9fca36f85621e1b4
Daren <darensin01@gmail.com>
2015-11-03 21:24:48 +0000

    Entered function line in matlab file

--- decision_tree.m
+++ decision_tree.m
@@ -2,3 +2,11 @@
 % x: Nx45 array, where N = #examples, 45 represents facial action units
 % y: Nx1 vector, representing labels of corresponding examples
 
+function[] = decision_tree(examples_matrix, attributes_vector, target_vector)
+
+
+
+
+
+
+
