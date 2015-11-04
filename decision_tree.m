d8ae89dcd63fb499f72058e1a8812812e1505355
Daren <darensin01@gmail.com>
2015-11-04 09:45:16 +0000

    Added basic structure for tree

--- decision_tree.m
+++ decision_tree.m
@@ -2,9 +2,34 @@
 % x: Nx45 array, where N = #examples, 45 represents facial action units
 % y: Nx1 vector, representing labels of corresponding examples
 
-function[] = decision_tree(examples_matrix, attributes_vector, target_vector)
+function[tree] = decision_tree(examples_matrix, ...
+                     attributes_vector, binary_targets)
 
+% tree.op: label for node (empty for leaf node)
+% tree.kids: cell array which contains subtrees (empty for leaf node)
+% tree.class: label for leaf node (empty for internal node)
 
+% If all of classifications are the same
+if all(binary_targets == binary_targets(1))
+    tree.op = [];
+    tree.kids = [];
+    tree.class = binary_targets(1);
+% If there is no more attribute to test for/classify
+elseif is_empty(attributes_vector)
+    tree.op = [];
+    tree.kids = [];
+    tree.class = majority_value(binary_targets);
+% If none of the cases happens, branch out node according to best attribute
+%   using information gain
+else best_attribute = choose_best_decision_attribute(examples_matrix, ...
+                          attributes_vector, binary_targets)
+    tree.op = best_attribute;
+    tree.class = [];
+    tree.kids = [0 1];
+    for value = 1:2
+        
+    end
+end
 
 
 
