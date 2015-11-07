8bd088e002b5220b5e380549983daddb5e7b6eb6
Daren <darensin01@gmail.com>
2015-11-07 17:21:02 +0000

    Finally understood info gain file, renamed some variables, added some comments

--- information_gain.m
+++ information_gain.m
@@ -1,11 +1,10 @@
 % Given matrix of examples and specified attribute, calculate information
 %   gain of that attribute.
-% Examples
-%   information_gain([1 0; 0 1], 1, [1, 0]) = 1
 function [gain] = information_gain(examples, attribute, binary_targets)
 
     % Extracts from example matrix, positive examples where attribute is 1.
-    % Creates a bitmap with either 1 or 0.
+    % Creates a bitmap that represents if the attribute in the row is
+    %   either 1 or 0.
     positive_examples = (examples(:, attribute) == 1);
     
     % Won't this mean that positive bitmap size and negative bitmap size
@@ -15,9 +14,12 @@ function [gain] = information_gain(examples, attribute, binary_targets)
     num_total_examples = size(examples, 1);
     num_positive_examples = sum(positive_examples);
     num_negative_examples = sum(negative_examples);
+    
+    positive_ratio = num_positive_examples / num_total_examples;
+    negative_ratio = num_negative_examples / num_total_examples;
         
     gain = entropy(binary_targets) ...
-        - (num_positive_examples / num_total_examples) * entropy(binary_targets(positive_examples)) ...
-        - (num_negative_examples / num_total_examples) * entropy(binary_targets(negative_examples));
+        - positive_ratio * entropy(binary_targets(positive_examples)) ...
+        - negative_ratio * entropy(binary_targets(negative_examples));
 end
 
