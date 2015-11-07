c033d70ef6ec6ca4fd82020180f8c060c01204e5
Daren <darensin01@gmail.com>
2015-11-07 16:57:47 +0000

    Added comments for choose_best_decision_attribute file

--- choose_best_decision_attribute.m
+++ choose_best_decision_attribute.m
@@ -1,12 +1,26 @@
-function [ ret ] = choose_best_decision_attribute( examples, attributes, binary_targets )
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-    ig = zeros(size(attributes, 1), 1);
-    for idx = 1:length(attributes)
-        ig(idx) = information_gain(examples, attributes(idx), binary_targets);
+% This function returns the attribute with the highest information gain
+%   in the attributes vector.
+
+function [best_attribute] = choose_best_decision_attribute(examples, ... 
+                                attributes, binary_targets)
+    
+    % Create a zero-ed row vector with same dimensions as attributes.
+    gain = zeros(size(attributes, 1), 1);
+    
+    % Calculate information gain for each attribute and store in gain.
+    % Delegates calculation of information gain to another function.
+    for attr = 1 : length(attributes)
+        gain(attr) = information_gain(examples, attributes(attr), ...
+                         binary_targets);
     end
     
-    [~, best_idx] = max(ig);
-    ret = attributes(best_idx);
+    % best_idx returns the column number which has the highest info gain.
+    % ~ returns the value of the information gain, but we are not concerned
+    %   about the value for this function; we just want the highest
+    %   information gain.
+    [~, best_idx] = max(gain);
+    
+    % Returns the attribute (number) that has the highest info gain.
+    best_attribute = attributes(best_idx);
 end
 
--- decision_tree.m
+++ decision_tree.m
@@ -1,9 +1,7 @@
 % Load clean data from Matlab data (cleandata_students.mat)
-%   x: Nx45 array, where N = #examples, 45 represents facial action units
-%   y: Nx1 vector, representing labels of corresponding examples
 
-function[tree] = decision_tree(examples_matrix, ...
-                     attributes_vector, binary_targets)
+function[tree] = decision_tree(examples_matrix, attributes_vector, ...
+                                   binary_targets)
 
     % tree.op: label for node (empty for leaf node)
     % tree.kids: cell array which contains subtrees (empty for leaf node)
@@ -23,10 +21,10 @@ function[tree] = decision_tree(examples_matrix, ...
         tree.kids = [];
         tree.class = majority_value(binary_targets);
         return;
-    % If none of the cases happens, branch out node according to best attribute
-    %   using information gain
+    % If none of the cases happens, branch out node according to best
+    %   attribute using information gain
     else
-        best_attribute = choose_best_decision_attribute(examples_matrix, ...
+        best_attribute = choose_best_decision_attribute(examples_matrix,...
                               attributes_vector, binary_targets);
         tree = [];
         tree.op = best_attribute;
@@ -34,9 +32,10 @@ function[tree] = decision_tree(examples_matrix, ...
         tree.class = [];
 
         for value_attribute = 0:1
-            % Extracts the rows in the example matrix as examples_i which has
-            %   value of best_attribute as value_attribute (0 or 1)
-            indices = (examples_matrix(:, best_attribute) == value_attribute);
+            % Extracts the rows in the example matrix as examples_i which
+            %  has value of best_attribute as value_attribute (0 or 1)
+            indices = (examples_matrix(:, best_attribute) ...
+                           == value_attribute);
             examples_i = examples_matrix(indices, :);
 
             % Contains the classification (yes or no) of emotion in 
@@ -44,8 +43,6 @@ function[tree] = decision_tree(examples_matrix, ...
             binary_targets_i = binary_targets(indices);
 
             % If there is no example that has a particular value_attribute
-            %   i.e. all examples have a particular value for the attribute.
-            %if (size(examples_i, 1) == 0)
             if isempty(examples_i)
                 tree = [];
                 tree.op = [];
