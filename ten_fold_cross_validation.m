9b5ccdb2b70a04ea98518cc7de0772a6c6ade8c3
Daren Sin <ds2912@ray38.doc.ic.ac.uk>
2015-11-09 11:59:24 +0000

    Added comments to ten cross file

--- ten_fold_cross_validation.m
+++ ten_fold_cross_validation.m
@@ -5,7 +5,11 @@ function[tree] = ten_fold_cross_validation(examples_matrix, ...
                     attributes_vector, binary_targets)
      tree = [];
      best_percentage = 0;
+     
+     % Index variable to traverse through binary_targets
      index = 1;
+     
+     % Train and pick the best tree using 10-fold cross validation.
      for i = 1 : 10
         [training_set test_set] = partition(examples_matrix, i);
         t = decision_tree(training_set, attributes_vector, binary_targets);
@@ -17,14 +21,22 @@ function[tree] = ten_fold_cross_validation(examples_matrix, ...
            end
            index = index + 1;
         end
+        
+        % Picks best tree based on tree that has the highest proportion
+        %   of correctly classified examples.
         percent =  correct_count / size(test_set, 1) * 100;
         if best_percentage < percent
             tree = t;
             best_percentage = percent;
         end
      end
+     
+     % Number of times the tree classifies the row as true (1)
      counter_yes = 0;
+     
+     % Number of times the row in the matrix gives true (1)
      counter_true = 0;
+     
      index = 1;
      for row = examples_matrix'
          if predict(tree, row)
@@ -33,10 +45,16 @@ function[tree] = ten_fold_cross_validation(examples_matrix, ...
          end
          index = index + 1;
      end
+     
      tree.prob_yes = counter_true/counter_yes;
+     
+     % Number of times the row in the matrix gives false (0)
      counter_false = 0;
+     
+     % Number of times the tree classifies the row as false
      counter_no = size(examples_matrix,1) - counter_yes;
      index = 1;
+     
      for row = examples_matrix'
          if predict(tree, row) == 0
              counter_false = counter_false + binary_targets(index);
@@ -46,6 +64,7 @@ function[tree] = ten_fold_cross_validation(examples_matrix, ...
      tree.prob_false = counter_false/counter_no;
 end
 
+% Helper function to split example matrix into test and training sets
 function [training_set test_set] = partition(examples_matrix, ith_partition)
     nrows = size(examples_matrix, 1);
     test_set_start_index = floor((ith_partition - 1) / 10 * nrows) + 1;
