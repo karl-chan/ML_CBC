3bdfa4216c392e680a5aeda105b2214c44872409
Karl Chan <karl.chan13@imperial.ac.uk>
2015-11-22 16:31:45 +0000

    Reorganised files

--- avg_classification_rate.m
+++
@@ -1,9 +0,0 @@
-function [ rate ] = avg_classification_rate( matrix )
-%AVG_CLASSFICATION_RATE Calculates average classification rate
-%   CR is no. of correctly classified examples (trace) divided by
-%   total no. of examples (sum of matrix)
-    total = sum(sum(matrix));
-    correct = trace(matrix);
-    rate = correct / total;
-end
-
--- majority_value.m
+++
@@ -1,5 +0,0 @@
-% Returns the mode of the binary_targets (either 0 or 1)
-
-function [majority_value] = majority_value(binary_targets)
-    majority_value = mode(binary_targets);
-end
