d22e8022834323ce7a97f6e8ef2fff0260da32ec
Karl Chan <karl.chan13@imperial.ac.uk>
2015-11-25 13:38:42 +0000

    Added ANN

--- code/artificial neural network/ten_fold_cross_validation.m
+++ code/artificial neural network/ten_fold_cross_validation.m
@@ -8,17 +8,19 @@ function[results] = ten_fold_cross_validation(x, y)
      
      % Train and pick the best tree using 10-fold cross validation.
      [x2, y2] = ANNdata(x, y);
-     for hidden_neurons = 5:20
-         cum_sum_confusion_matrix = zeros(6);
-         for i = 1 : 10 
-            [train_idx validation_idx] = partition(i);
-            net = create_network(x, y, 2, hidden_neurons, 100, train_idx, validation_idx, []);     
-            predicted = testANN(net, x2(:, validation_idx), @NNout2labels);
-            cum_sum_confusion_matrix = cum_sum_confusion_matrix ...
-                  + confusion_matrix(6, predicted, y(validation_idx));
-         end
-         avg_confusion_matrix = cum_sum_confusion_matrix / 10
-         results = [results; mean(f1_measure(avg_confusion_matrix))];
+     for first_neurons = 5:5:30
+        for second_neurons = 5:5:30
+             cum_sum_confusion_matrix = zeros(6);
+             for i = 1 : 10 
+                [train_idx validation_idx] = partition(i);
+                net = create_network(x, y, 3, [first_neurons second_neurons], 100, train_idx, validation_idx, []);     
+                predicted = testANN(net, x2(:, validation_idx), @NNout2labels);
+                cum_sum_confusion_matrix = cum_sum_confusion_matrix ...
+                      + confusion_matrix(6, predicted, y(validation_idx));
+             end
+             avg_confusion_matrix = cum_sum_confusion_matrix / 10
+             results = [results; mean(f1_measure(avg_confusion_matrix))];
+        end
      end     
 end
 
