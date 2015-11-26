d5df41a4c2e29c08a2b09ee604a7437401d4d2ff
Karl Chan <karl.chan13@imperial.ac.uk>
2015-11-26 10:45:03 +0000

    Added traingdm results

--- code/artificial neural network/ten_fold_cross_validation.m
+++ code/artificial neural network/ten_fold_cross_validation.m
@@ -8,18 +8,18 @@ function[results] = ten_fold_cross_validation(x, y)
      
      % Train and pick the best tree using 10-fold cross validation.
      [x2, y2] = ANNdata(x, y);
-     
-         for hidden_neurons = 5 : 5 : 50
+     for hidden_neurons = 5 : 5 : 50
              cum_sum_confusion_matrix = zeros(6);         
              for i = 1 : 10 
                 [train_idx validation_idx] = partition(i);
-                net = create_traingdm(x, y, 2, hidden_neurons, 1000, train_idx, validation_idx, [], 0.6, 0.9);     
+                net = create_traingdm(x, y, 2, hidden_neurons, 10000, train_idx, validation_idx, [], 0.9, 0.6);     
                 predicted = testANN(net, x2(:, validation_idx), @NNout2labels);
                 cum_sum_confusion_matrix = cum_sum_confusion_matrix ...
                       + confusion_matrix(6, predicted, y(validation_idx));
-             end        
+             end  
              avg_confusion_matrix = cum_sum_confusion_matrix / 10
              results = [results; mean(f1_measure(avg_confusion_matrix))]
-         end
+     end
+
 end
 
