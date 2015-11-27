c5b4ecb8edfa770dacc15debeac73d7de066b2da
Jia Zheng <jz7213@ray29.doc.ic.ac.uk>
2015-11-27 17:18:19 +0000

    Added results for traingda

--- code/artificial neural network/ten_fold_cross_validation.m
+++ code/artificial neural network/ten_fold_cross_validation.m
@@ -12,7 +12,7 @@ function[results] = ten_fold_cross_validation(x, y)
              cum_sum_confusion_matrix = zeros(6);         
              for i = 1 : 10 
                 [train_idx validation_idx] = partition(i);
-                net = create_traingdm(x, y, 2, hidden_neurons, 10000, train_idx, validation_idx, [], 0.9, 0.6);     
+                net = create_traingda(x, y, 2, hidden_neurons, 10000, train_idx, validation_idx, [], 0.4, 1.004, 0.4);     
                 predicted = testANN(net, x2(:, validation_idx), @NNout2labels);
                 cum_sum_confusion_matrix = cum_sum_confusion_matrix ...
                       + confusion_matrix(6, predicted, y(validation_idx));
